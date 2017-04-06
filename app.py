549f1652bf98e322e5b8ec0866827b37da990319
bigpythonimish <nimishverma@ymail.com>
2017-04-06 15:57:44 +0530

    fixed importing errors
    you dont have to import whole urllb.request if you are using urlopen() only, which you already have imported in 5th line.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask , render_template,json,jsonify , request,redirect , url_for,session
-import urllib.request
 from urllib.request import urlopen
 
 
@@ -7,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def change():
-    a=urllib.request.urlopen('http://api.fixer.io/latest')
+    a=urlopen('http://api.fixer.io/latest')
     b=[]
     data=json.loads(a.read())
 
